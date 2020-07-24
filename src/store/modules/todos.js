a115c529da84863d010b14f2bf038b6438e6a58b
Loquen Jones <loquenjones@Loquens-MacBook-Pro.local>
2020-07-24 15:02:43 -0700

    intial commit

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -13,12 +13,25 @@ const actions = {
     const res = await axios.get('https://jsonplaceholder.typicode.com/todos')
 
     commit('setTodos', res.data)
+  },
+
+  async addTodo({ commit }, title) {
+    const res = await axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed: false})
+
+    commit('newTodo', res.data)
+  },
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+
+    commit('removeTodo', id)
   }
 
 }
 
 const mutations = {
-  setTodos: (state, todos) => (state.todos = todos)
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => (state.todos.unshift(todo)),
+  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id != id)
 }
 
 export default {
